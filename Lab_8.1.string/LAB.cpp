a3f7323d8e03161b9bd4762d7b282d44992021d6
mrian11 <marian.yatsyk.ri.2023@lpnu.ua>
2023-11-15 15:27:54 +0200

    1

---
+++ Lab_8.1.string/LAB.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int Count(const string s)
+{
+	int k = 0;
+	size_t pos = 0;
+	while ((pos = s.find(',', pos)) != -1)
+	{
+		pos++;
+		if (s[pos] == '-')
+			k++;
+	}
+	return k;
+}
+string Change(string& s)
+{
+	size_t pos = 0;
+	while ((pos = s.find(',', pos)) != -1)
+		if (s[pos + 1] == '-')
+			s.replace(pos, 2, "*");
+		else
+			pos++;
+	return s;
+}
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+
+		getline(cin, str);
+	cout << "String contained " << Count(str) << " groups of '+ -'" << endl;
+	string dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+	return 0;
+}
---
+++ UnitTest8.1.string/UnitTest8.1.string.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include"../Lab_8.1.string/LAB.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81string
+{
+	TEST_CLASS(UnitTest81string)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(Count("d,-g..g,-"), 2);
+		}
+	};
+}
